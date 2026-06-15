b205e87e97c94e8a2d48f8b6038a0ba92d2e1ccf
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-06-15 10:30:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -146,6 +146,7 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add("active");
+            observer.unobserve(entry.target);
         }
     });
 }, {
